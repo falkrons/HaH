c8aa2c9cb1087817f2330be25429f44a9aa67190
Steven Vergenz <vergenzs@gmail.com>
2016-06-03 15:21:59 -0400

    Add basic Google Analytics tracking

--- client/client.js
+++ client/client.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// do google analytics tracking
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-78752535-1', 'auto');
+ga('send', 'pageview');
+
+
 // force define String.trim
 if(typeof(String.prototype.trim) === "undefined")
 {
