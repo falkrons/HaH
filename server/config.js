adba19a58de4f99693bde2cee42795b5631194ba
Brian Peiris <brianpeiris@gmail.com>
2016-08-12 13:56:43 -0700

    Change default port

--- server/config.js
+++ server/config.js
@@ -8,7 +8,7 @@ catch (e) {
 }
 // defaults for config
 module.exports = {
-	port: jsonConfig.port || process.env.PORT || 7878,
+	port: jsonConfig.port || process.env.PORT || 80,
 	minPlayers: jsonConfig.minPlayers || 4,
 	maxPlayers: jsonConfig.maxPlayers || 12,
 	syncInterval: jsonConfig.syncInterval || 100,
