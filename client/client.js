e1f836feaa9c3645b8038d850d7b523f87cbb7fd
WACOMalt <wacomalt@gmail.com>
2016-11-17 18:04:26 -0500

    Allow disabling of the popup button with a URL string
    add &noPopup to the URL to disable the popup button on demand.

--- client/client.js
+++ client/client.js
@@ -85,9 +85,11 @@ if( altspace.inClient )
 		}
 	});
 
-	altspace.open(
-		'https://docs.google.com/forms/d/1UHU5_WUsz9hoDdkiyiouvl5u49NgNUSA_ZS6qyXOtJk/viewform',
-		'_experience', {icon: window.location.origin+'/static/icon.png', hidden: true});
+	if( ! /[&?]noPopup/.test(window.location.search) ){
+		altspace.open(
+			'https://docs.google.com/forms/d/1UHU5_WUsz9hoDdkiyiouvl5u49NgNUSA_ZS6qyXOtJk/viewform',
+			'_experience', {icon: window.location.origin+'/static/icon.png', hidden: true});
+	}
 }
 else
 {
